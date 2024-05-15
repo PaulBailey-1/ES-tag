24b80021041f997b44aa12e4eb3a9bca962248f3
PaulBailey-1 <paul.ryan.bailey@gmail.com>
2024-05-15 10:40:28 -0500

    CMA implemented

--- trainer/src/optimizer.py
+++ trainer/src/optimizer.py
@@ -15,19 +15,31 @@ def normalize(vec):
 class Optimizer:
 
     def __init__(self, params, lamb, logDir, config=None):
-        self.params = params
+        self.x = params
         self.n = len(params)
         # self.mu = lamb / 7
         self.lamb = lamb
         self.mu = 1
-        self.sigma = 0.01
+        self.delta = 0.01
+        self.c = 1 / np.sqrt(self.n)
+        self.ccov = 2 / self.n ** 2
+        self.beta = 1 / self.n
+        self.chiHat = np.sqrt(self.n) * (1 - (1 / (4 * self.n)) + (1 / (21 * self.n ** 2)))
 
+        self.z = np.zeros(self.n)
+        self.B = np.identity(self.n)
+        self.C = np.identity(self.n)
+        self.s = np.zeros(self.n)
+        self.sNorm = np.zeros(self.n)
         self.step = np.zeros(self.n)
         self.lastStep = np.zeros(self.n)
 
         if config:
-            self.mu = config['mu']
-            self.sigma = config['sigma']
+            if config['mu']: self.mu = config['mu']
+            if config['delta0']: self.delta = config['delta0']
+            if config['c']: self.c = config['c']
+            if config['ccov']: self.ccov = config['ccov']
+
             if self.mu > self.lamb:
                 print("Error: Parent population cannot be larger than total population")
 
@@ -38,21 +50,38 @@ class Optimizer:
         self.log_writer = tf.summary.create_file_writer(logDir + "/evolution")
         self.log_writer.set_as_default()
 
-        print(f"Initialized optimizer n: {self.n} mu: {self.mu} sigma: {self.sigma}")
+        print(f"Initialized optimizer n: {self.n} mu: {self.mu} sigma: {self.delta}")
 
     def getParams(self):
         noise = rng.normal(size=self.n)
         self.noise_table.append(noise)
-        params = self.params + self.sigma * noise
-        return params #, len(self.noise_table) - 1
+        params = self.x + self.delta * np.dot(self.B, noise)
+        return params
+    
 
+    # CMA-ES: https://ieeexplore.ieee.org/document/542381
     def update(self, generation, rewards):
+
         sorting = np.array(rewards).argsort()[::-1][:self.mu]
+        self.z = np.zeros(self.n)
         for i in range(self.mu):
-            self.step += self.w[i] * self.noise_table[sorting[i]]
-        self.step *= self.sigma
-        self.params += self.step
+            self.z += self.w[i] * self.noise_table[sorting[i]]
+        self.step = self.delta * np.dot(self.B, self.z)
+        self.x += self.step
         self.noise_table.clear()
+
+        eigvals, eigvecs = np.linalg.eigh(self.C)
+        Bnorm = np.copy(eigvecs)
+        for i in range(self.n):
+            eigvecs[:,i] *= np.sqrt(eigvals[i])
+        self.B = eigvecs
+        cu = np.sqrt(self.c * (2 - self.c))
+
+        self.s = (1 - self.c) * self.s + cu * np.dot(self.B, self.z)
+        self.sNorm = (1 - self.c) * self.sNorm + cu * np.dot(Bnorm, self.z)
+
+        self.C = (1 - self.ccov) * self.C + self.ccov * np.dot(self.s, self.s.T)
+        self.delta = self.delta * np.exp(self.beta * (np.linalg.norm(self.sNorm) - self.chiHat))
 
         avgReward = 0
         for i in sorting:
@@ -61,9 +90,9 @@ class Optimizer:
 
         stepCorrelation = np.dot(normalize(self.step), normalize(self.lastStep))
         self.lastStep = np.copy(self.step)
-        print(stepCorrelation)
             
         tf.summary.scalar('top reward', data=rewards[sorting[0]], step=generation)
         tf.summary.scalar('mean reward', data=avgReward, step=generation)
-        tf.summary.scalar('step size', data=self.sigma, step=generation)
+        tf.summary.scalar('delta', data=self.delta, step=generation)
+        tf.summary.scalar('path length', data=np.linalg.norm(self.sNorm), step=generation)
         tf.summary.scalar('step correlation', data=stepCorrelation, step=generation)

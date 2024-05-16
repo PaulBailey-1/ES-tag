910febb643189d8135214b0de28e5215078ffe47
PaulBailey-1 <paul.ryan.bailey@gmail.com>
2024-05-16 09:30:22 -0500

    Cleaning up

--- trainer/push.bat
+++
@@ -1,4 +0,0 @@
-docker build -t "tag-trainer" .
-docker tag tag-trainer 339713009859.dkr.ecr.us-east-1.amazonaws.com/tag-trainer
-aws ecr get-login-password | docker login --username AWS --password-stdin 339713009859.dkr.ecr.us-east-1.amazonaws.com
-docker push 339713009859.dkr.ecr.us-east-1.amazonaws.com/tag-trainer

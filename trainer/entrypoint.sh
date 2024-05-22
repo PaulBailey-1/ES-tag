aa36b88f89094442458511483325dcd6938adc3d
PaulBailey-1 <paul.ryan.bailey@gmail.com>
2024-05-22 17:57:37 -0500

    Changes for training with docker locally

--- trainer/entrypoint.sh
+++ trainer/entrypoint.sh
@@ -18,6 +18,7 @@ export WORKERS_PER_NODE=1
   --mca btl_tcp_if_exclude lo,ecs-eth0 \
   -hostfile /hostfile-ompi \
   -n $(($AWS_BATCH_JOB_NUM_NODES * $WORKERS_PER_NODE)) \
-  python main.py -g http://localhost:5000 -l /mount/efs/logs/ -c /mount/efs/taggerConfig.json
+  python main.py -g http://game-server-RANK:5000 -l /app/logs/ -c taggerConfig.json
+  # python main.py -g http://localhost:5000 -l /mount/efs/logs/ -c taggerConfig.json
 
 # CMD mpiexec --allow-run-as-root -n 1 python main.py "http://game-server:5000"
